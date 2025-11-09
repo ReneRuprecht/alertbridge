9f8ad2ddb49b11eeb16cd3ff3eb62c173c359452
rene <rene.ruprecht.396@gmail.com>
2025-11-09 22:29:07 +0100

    fix(backend): add cors middleware to allow frontend requests

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.services.alert_service import fetch_alerts, fetch_and_store_alerts
 
 app = FastAPI()
 
+origins = [
+        "http://localhost:5173"
+        ]
+
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins
+        )
 
 @app.get("/")
 async def root():
