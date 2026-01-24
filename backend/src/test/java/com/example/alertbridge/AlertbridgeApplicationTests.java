2aeb1f6bfa4cef747cfa88ed39b7ec7f4007abed
rene <rene.ruprecht.396@gmail.com>
2026-01-24 21:48:23 +0100

    basic spring boot project

---
+++ backend/src/test/java/com/example/alertbridge/AlertbridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alertbridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertbridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
