487605e35825f70666935bba1cdb6dd50a6f32fa
rene <rene.ruprecht.396@gmail.com>
2026-02-06 23:48:00 +0100

    chore(frontend): add .env and dockerfile.prod

--- frontend/src/api/GetCurrentAlerts.ts
+++ frontend/src/api/GetCurrentAlerts.ts
@@ -1,7 +1,8 @@
 import type { AlertState } from "../types/Alert";
 
 export async function getCurrentAlerts(): Promise<AlertState[]> {
-  const response = await fetch("http://localhost:8080/api/v1/alert-states");
+  const base_api_url = import.meta.env.VITE_API_BASE_URL;
+  const response = await fetch(base_api_url + "/api/v1/alert-states");
   if (!response.ok) throw new Error("Failed to fetch alert states");
   return response.json();
 }
