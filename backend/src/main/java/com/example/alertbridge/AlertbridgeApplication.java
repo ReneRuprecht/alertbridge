0955ff617607b67910def36e0c2f046162148686
rene <rene.ruprecht.396@gmail.com>
2026-07-04 16:36:52 +0200

    chore(backend): bootstrap spring boot application

---
+++ backend/src/main/java/com/example/alertbridge/AlertbridgeApplication.java
@@ -0,0 +1,13 @@
+package com.example.alertbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertbridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertbridgeApplication.class, args);
+	}
+
+}
